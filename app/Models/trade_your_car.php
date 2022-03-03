2cb7a16762a94704d0cd10e124280a69dc818aa3
ivakholeksii <oleksiiivakh@gmail.com>
2022-03-03 08:10:20 -0600

    Update to owner auction list end point

--- app/Models/trade_your_car.php
+++ app/Models/trade_your_car.php
@@ -11,7 +11,7 @@ class trade_your_car extends Model
     use HasFactory;
     protected $table ='trade_your_car';
     protected $fillable = ['vin', 'odometer', 'transmission', 'trim', 'drivetrain', 'engine', 'fuel_type', 'year', 'make', 'model', 'color', 'user_id', 'body_type', 'condition', 'exterior_color', 'state', 'city', 'zip', 'phone', 'vehicle_driving', 'transmission_issue', 'drivetrain_issue', 'steering_issue', 'brake_issue', 'suspension_issue', 'minor_body_damage', 'scratches', 'glass_damaged_cracked', 'lights_damaged_cracked', 'minor_body_rust', 'moderate_body_rust', 'major_body_rust', 'aftermarket_parts_exterior', 'mismatched_paint_colors', 'previous_paint_work', 'seat_damage', 'carpet_damage', 'dashboard_damage', 'interior_trim_damage', 'sunroof', 'navigation', 'aftermarket_stereo_equipment', 'hvac_not_working', 'leather_Or_Leather_type_seats', 'shoping_make', 'shoping_model', 'radius', 'loan_or_lease_on_car', 'car_keys', 'reserve_price', 'tradersell_inspected', 'latitude', 'longitude', 'status'];
-    
+
     public $timestamps = false;
 
     function get_images(){
@@ -22,5 +22,4 @@ class trade_your_car extends Model
         return $this->hasMany('App\Models\auction_bids', 'auction_item_id', 'id');
     }
 
-    
 }
