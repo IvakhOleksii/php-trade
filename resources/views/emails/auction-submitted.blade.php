c6c3c9c198c19b6aa6c7925cc412db2329537614
juansoteldo <jsoteldoup@gmail.com>
2022-03-24 21:15:46 -0500

    add email notification for auction

---
+++ resources/views/emails/auction-submitted.blade.php
@@ -0,0 +1,12 @@
+<h2>Your auction has been updated</h2>
+
+<hr>
+
+<p>
+  Dear {{ $name }},<br><br>
+  The auction has changed. Here you go.<br><br>
+  <b>Year</b>: {{ $info['year'] }}<br>
+  <b>Model</b>: {{ $info['model'] }}<br>
+  <b>Make</b>: {{ $info['make'] }}<br>
+  <b>Mileage</b>: {{ $info['mileage'] }}
+</p>
