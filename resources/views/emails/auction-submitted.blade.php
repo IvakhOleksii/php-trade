20d4c104e283a02c2660c9f0dfb8728144f6aa80
ivakholeksii <oleksiiivakh@gmail.com>
2022-03-29 19:47:05 -0500

    add email notification for new auction bid

--- resources/views/emails/auction-submitted.blade.php
+++ resources/views/emails/auction-submitted.blade.php
@@ -5,8 +5,20 @@
 <p>
   Dear {{ $name }},<br><br>
   The auction has changed. Here you go.<br><br>
+
+  @if ($info['year'])
   <b>Year</b>: {{ $info['year'] }}<br>
+  @endif
+
+  @if ($info['model'])
   <b>Model</b>: {{ $info['model'] }}<br>
+  @endif
+
+  @if ($info['make'])
   <b>Make</b>: {{ $info['make'] }}<br>
+  @endif
+
+  @if ($info['mileage'])
   <b>Mileage</b>: {{ $info['mileage'] }}
+  @endif
 </p>
