0add729b42f0572b55c3fdc56de9c1ecff86afbd
ivakholeksii <oleksiiivakh@gmail.com>
2022-04-27 18:22:44 -0500

    Added  https://www.zipcodeapi.com get zip codes by radius.

--- config/constants.php
+++ config/constants.php
@@ -4,4 +4,8 @@ return array(
     'pagination' => array(
            'items_per_page' => 2
     ),
+    'zipcode_api' => array(
+        'base_url' => 'https://www.zipcodeapi.com/rest/BfiiKfzMisnXfP1pEvHlgAuhf3HqT3qMxJf71qNQyhGQl0aO60IausEEqn5sWL7c/radius.json/',
+        'key' => 'BfiiKfzMisnXfP1pEvHlgAuhf3HqT3qMxJf71qNQyhGQl0aO60IausEEqn5sWL7c'
+ ),
 );
